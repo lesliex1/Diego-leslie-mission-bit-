2382ed09c218cbdd97444d61b6cb5dc26f7aa373
lesliex1 <83678246+lesliex1@users.noreply.github.com>
2023-05-02 17:20:52 -0700

    background song

---
+++ parkour maze/maze run/Assets/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("dead"))
+        {
+            transform.position = new Vector3(-6.29f,-0.81f,59.47f);
+        }
+
+    }
+
+}
